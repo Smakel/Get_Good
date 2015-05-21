a720006463f98bd926dafd41de11ec7e8638dfef
Smakel <schneicw@gmail.com>
2015-05-21 04:19:07 -0500

    toast added

--- mobile/src/main/java/com/schneider/cody/getgood/MainActivity.java
+++ mobile/src/main/java/com/schneider/cody/getgood/MainActivity.java
@@ -3,22 +3,18 @@ package com.schneider.cody.getgood;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
-
+import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
@@ -29,7 +25,6 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     private Button gbutton;
     ArrayList<String> list = new ArrayList<String>();
     ArrayAdapter<String> gadapter;
-
 
 
     @Override
@@ -43,7 +38,7 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         gedit = (EditText)findViewById(R.id.goal_edit);
         gadapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, list);
 
-        // set the lv variable to your list in the xml
+
         glist=(ListView)findViewById(R.id.g_list);
         glist.setAdapter(gadapter);
 
@@ -56,47 +51,55 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         if(input.length() > 0) {
             // add string to the adapter, not the listview
             gadapter.add(input);
-            // no need to call adapter.notifyDataSetChanged(); as it is done by the adapter.add() metho
 
 
-            glist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                // setting onItemLongClickListener and passing the position to the function
+
+        glist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
 
                 public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
 
 
                     // method to remove list item
 
-                    AlertDialog.Builder alert = new AlertDialog.Builder(
+            AlertDialog.Builder alert = new AlertDialog.Builder(
                             MainActivity.this);
 
-                    alert.setTitle("Complete Goal");
-                    alert.setMessage("Are you finished with this goal?");
-                    AlertDialog.Builder yes = alert.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            alert.setTitle("Complete Goal");
+            alert.setMessage("Are you finished with this goal?");
+            AlertDialog.Builder yes = alert.setPositiveButton("YES", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                            // TOD O Auto-generated method stub
+
 
                             // main code on after clicking yes
 
                             list.remove(position);
                             gadapter.notifyDataSetChanged();
                             gadapter.notifyDataSetInvalidated();
+                            Context context = getApplicationContext();
+                            CharSequence text = "Congratulations!";
+                            int duration = Toast.LENGTH_SHORT;
+                            Toast toast = Toast.makeText(context,text,duration);
+                            toast.show();
 
                         }
                     });
                     alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
+
+                            Context context = getApplicationContext();
+                            int duration = Toast.LENGTH_SHORT;
+                            CharSequence text = "Keep Trying";
+                            Toast toast = Toast.makeText(context,text,duration);
+                            toast.show();
                             dialog.dismiss();
                         }
                     });
 
-                    alert.show();
-
-    }});
-        }}}
-
+                    alert.show(); }})
+            ;}}}
 
 
 
